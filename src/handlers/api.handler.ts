b0f155b9d6c89c05bd7d8cb52ab36b7794d31b69
Michael <msquaremega@gmail.com>
2023-05-19 09:20:00 +0100

    activity done

--- src/handlers/api.handler.ts
+++ src/handlers/api.handler.ts
@@ -4,49 +4,53 @@ import Activity from '../models/activity.model';
 
 interface ApiResponse {
     events: any[];
-    continuation?: string;
-  }
+    continuation: string;
+}
 
-const fetchEvent =async () => {
+const fetchEvent = async () => {
     const baseUrl = 'https://api.reservoir.tools/events/asks/v3';
     const limit = 1000;
     let continuation = "";
     let events: any[] = [];
-    
-    const queryParams = `?limit=${limit}${continuation ? `&continuation=${continuation}` : ''}`;
+
+    while (true) {
+        const queryParams = `?limit=${limit}${continuation ? `&continuation=${continuation}` : ''}`;
         const url = `${baseUrl}${queryParams}`;
 
         try {
             const res = await axios.get(url); // fetch(url);
-            const dataToJson= (await res.data as ApiResponse);
+            const dataToJson = (await res.data as ApiResponse);
+            continuation = dataToJson.continuation;
             const newOrderEvents = dataToJson.events.filter((event: any) => event.event.kind == 'new-order')
             await createActivity(newOrderEvents);
             if (continuation == "") {
-                return;
+                break
             }
         } catch (error) {
             console.error(`Error fetching data from the api: ${error}`);
-            return;
+            break;
         }
+    }
 
     return events;
 }
 
-const createActivity =async (data:any) => {
-    Activity.create({
-        contract_address: data.order.contract,
-        token_index: data.order.criteria.data.token.tokenId,
-        listing_price: data.order.price.amount.native,
-        maker: data.order.maker,
-        listing_from: data.order.validFrom,
-        listing_to: data.order.validTo,
-        event_timestamp: data.event.createdAt,
-    }).then(res => {
-        console.log(`Activity with id: ${res.id}, created successfully`);
-    }).catch((error) => {
-        console.error(`Error creating activity: ${error}`)
-    })
+const createActivity = async (datas: any) => {
+    for (let data of datas.event) {
+        Activity.create({
+            contract_address: data.order.contract,
+            token_index: data.order.criteria.data.token.tokenId,
+            listing_price: data.order.price.amount.native,
+            maker: data.order.maker,
+            listing_from: data.order.validFrom,
+            listing_to: data.order.validTo,
+            event_timestamp: data.event.createdAt,
+        }).then(res => {
+            console.log(`Activity with id: ${res.id}, created successfully`);
+        }).catch((error) => {
+            console.error(`Error creating activity: ${error}`)
+        })
+    }
 }
 
 export default fetchEvent;
-
