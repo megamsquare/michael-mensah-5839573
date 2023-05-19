d1bb8364530c1e3ca48477f23a2435cc1507cb9e
Michael <msquaremega@gmail.com>
2023-05-19 12:24:53 +0100

    correction done

--- src/controllers/listening.controller.ts
+++ src/controllers/listening.controller.ts
@@ -3,43 +3,47 @@ import Activity from "../models/activity.model";
 import Token from "../models/token.model";
 
 async function processListings() {
-    try {
-        // Find the most recent listing for each token
+  try {
+    // Find the most recent listing for each token
 
-        const activities = await Activity.findAll();
-        const tokens = await Token.findAll();
+    const activities = await Activity.findAll();
+    const tokens = await Token.findAll();
 
-        for (const token of tokens) {
-            const tokenActivities = activities.filter(
-                (activity) =>
-                  activity.contract_address === token.contract_address &&
-                  activity.token_index === token.index
-              );
+    if (tokens.length !== 0) {
+      for (const token of tokens) {
+        const tokenActivities = activities.filter(
+          (activity) =>
+            activity.contract_address === token.contract_address &&
+            activity.token_index === token.index
+        );
 
-              if (tokenActivities.length !== 0) {
-                for (let tokenActivity of tokenActivities ) {
-                  await createToken(tokenActivity);
-                }
-              }
+        if (tokenActivities.length !== 0) {
+          for (let tokenActivity of tokenActivities) {
+            await createToken(tokenActivity);
+          }
         }
+      }
 
-        console.log('Listings processed successfully');
-    } catch (error) {
-        console.error('Error processing listings:', error);
+    } else {
+      await createToken(activities);
     }
+    console.log('Listings processed successfully');
+  } catch (error) {
+    console.error('Error processing listings:', error);
+  }
 }
 
-const createToken =async (data:any) => {
+const createToken = async (data: any) => {
   Token.create({
     index: data.token_index,
-    contract_address:data.contract_address
+    contract_address: data.contract_address
   })
     .then((res) => {
-    console.log(`Token with id: ${res.id}, created successfully`)
-  })
-  .catch((error) => {
-    console.error(`Error creating token: ${error}`)
-  })
+      console.log(`Token with id: ${res.id}, created successfully`)
+    })
+    .catch((error) => {
+      console.error(`Error creating token: ${error}`)
+    })
 }
 
 export { processListings };
